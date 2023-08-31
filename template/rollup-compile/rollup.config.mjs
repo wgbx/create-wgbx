41d4ef9e66a551560e16ea1995d3a0c68b210ec0
微光北下 <wgbx@foxmail.com>
2023-08-31 16:19:49 +0800

    feat: 调整rollup-compile ts方案

--- template/rollup-compile/rollup.config.mjs
+++ template/rollup-compile/rollup.config.mjs
@@ -1,8 +1,7 @@
 import NodeResolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import babel from '@rollup/plugin-babel'
 import terser from '@rollup/plugin-terser'
-import typescript from '@rollup/plugin-typescript'
+import typescript from 'rollup-plugin-typescript2'
 
 const name = 'packagesName'
 
@@ -29,10 +28,6 @@ export default {
   plugins: [
     NodeResolve(),
     commonjs(),
-    babel({
-      babelHelpers: 'bundled',
-      exclude: 'node_modules/**'
-    }),
-    typescript({ outDir: 'dist/' })
+    typescript()
   ],
 }
